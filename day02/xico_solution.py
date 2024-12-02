6f01636b08bb4b5f09b540badbf03fedaf873815
fsantos98 <franciscoslb.98@hotmail.com>
2024-12-02 20:20:48 +0000

    some stuff

--- day02/xico_solution.py
+++ day02/xico_solution.py
@@ -8,11 +8,7 @@ def open_file():
     return reports
 
 def part1():
-    
-    start_time = time.perf_counter()
     reports = open_file()
-    elapsed_time = (time.perf_counter() - start_time) * 1000
-    print(f"Elapsed time reading file: {elapsed_time:.4f} ms")
 
     total_safe_reports = 0
     for report in reports:
@@ -35,13 +31,37 @@ def part1():
 
         if valid:
             total_safe_reports += 1  
-
+        
     return total_safe_reports       
 
-
-
 def part2():
-    pass
+    reports = open_file()
+
+    total_safe_reports = 0
+    for report in reports:
+        valid = True
+        old_state, new_state = 0, 0
+        ignore_index = -1
+        for i in range(0, len(report) - 1):
+            if not valid:
+                continue
+
+            curr_diff = report[i] - report[i + 1]
+
+            if curr_diff < 0:
+                new_state = -1
+            elif curr_diff > 0:
+                new_state = 1;
+        
+            if abs(curr_diff) > 3 or (i > 0 and (new_state != old_state or curr_diff == 0 )):
+                valid = False
+            
+            old_state = new_state
+
+        if valid:
+            total_safe_reports += 1  
+        
+    return total_safe_reports  
 
 print(part1())
-part2()
+print(part2())
