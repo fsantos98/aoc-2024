83fc356a85e990e09e38614346438b472d2b469e
fsantos98 <franciscoslb.98@hotmail.com>
2024-12-03 00:56:42 +0000

    day 02 and bugfixes

--- day02/xico_solution.py
+++ day02/xico_solution.py
@@ -7,61 +7,49 @@ def open_file():
             reports.append([int(char) for char in line.strip().split(" ")])
     return reports
 
+def isReportValid(report):
+    old_state, new_state = 0, 0
+    for i in range(1, len(report)):            
+        curr_diff = report[i] - report[i - 1]
+
+        if curr_diff < 0:
+            new_state = -1
+        elif curr_diff > 0:
+            new_state = 1;
+    
+        if abs(curr_diff) > 3 or (i > 1 and (new_state != old_state or curr_diff == 0 )):
+            return False
+        
+        old_state = new_state
+
+    return True
+
 def part1():
     reports = open_file()
 
     total_safe_reports = 0
     for report in reports:
-        valid = True
-        old_state, new_state = 0, 0
-        for i in range(1, len(report)):            
-            if not valid:
-                continue
-            curr_diff = report[i] - report[i - 1]
-
-            if curr_diff < 0:
-                new_state = -1
-            elif curr_diff > 0:
-                new_state = 1;
-        
-            if abs(curr_diff) > 3 or (i > 1 and (new_state != old_state or curr_diff == 0 )):
-                valid = False
-            
-            old_state = new_state
-
-        if valid:
-            total_safe_reports += 1  
-        
+        total_safe_reports += isReportValid(report)
     return total_safe_reports       
+
 
 def part2():
     reports = open_file()
 
     total_safe_reports = 0
     for report in reports:
-        valid = True
-        old_state, new_state = 0, 0
-        ignore_index = -1
-        for i in range(0, len(report) - 1):
-            if not valid:
-                continue
+        if isReportValid(report):
+            total_safe_reports += 1
+        else:
+            for i in range(0, len(report)):
+                removed_element = report.pop(i)
+                if isReportValid(report):
+                    total_safe_reports += 1
+                    break
+                else:
+                    report.insert(i, removed_element)
 
-            curr_diff = report[i] - report[i + 1]
-
-            if curr_diff < 0:
-                new_state = -1
-            elif curr_diff > 0:
-                new_state = 1;
-        
-            if abs(curr_diff) > 3 or (i > 0 and (new_state != old_state or curr_diff == 0 )):
-                valid = False
-            
-            old_state = new_state
-
-        if valid:
-            total_safe_reports += 1  
-        
-    return total_safe_reports  
+    return total_safe_reports       
 
 print(part1())
 print(part2())
---
+++ leaderboards/day02.md
@@ -0,0 +1,16 @@
+# Leaderboard for Day 2
+
+## 1
+
+| Person | Runtime (ms) |
+|--------|--------------|
+| Xico | **1.5854 ms** |
+| Luis | 1.7032 ms (+7.43%) |
+
+## 2
+
+| Person | Runtime (ms) |
+|--------|--------------|
+| Xico | **2.6064 ms** |
+| Luis | 4.2964 ms (+64.84%) |
+
